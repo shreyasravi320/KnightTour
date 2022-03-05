48b674cd18696dad2d79e34ffffa0681e7d252da
Shreyas Ravi <shreyasravi@shreyasravismba.fios-router.home>
2022-03-04 23:43:31 -0500

    Implemented GUI for viewing paths

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,15 @@ int main(int argc, char **argv)
 {
     populateSolutions();
 
-    Grid<int> grid(22, 22, UNVISITED);
-    solveClosedTour(grid);
-    // Viewer viewer = Viewer(30);
+    // Grid<int> grid(22, 22, UNVISITED);
+    // solveClosedTour(grid);
+    Viewer viewer = Viewer(12);
 
-    // while (viewer.getWindowIsOpen())
-    // {
-    //     viewer.update();
-    //     viewer.render();
-    // }
+    while (viewer.getWindowIsOpen())
+    {
+        viewer.update();
+        viewer.render();
+    }
 
     return 0;
 }
